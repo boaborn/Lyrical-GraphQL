ac78b490cb8b83f920d492a55ab5156ec5fb0627
Michael Zhao <Michael.Zhao@optimationgroup.com>
2019-01-09 12:32:00 +1300

    add create song form

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import ApolloClient from 'apollo-client' // ApolloClinent actually interacting with GraphQL server, making request for data and storing data locally when data comes back
 import { ApolloProvider } from 'react-apollo' // AppolloProvider knows how to communicate with react and ApolloClient
+
+import App from './components/App'
 import SongList from './components/SongList'
+import SongCreate from './components/SongCreate'
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={ client }>
-      <div>
-        <SongList/>
-      </div>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ SongList } />
+          <Route path="song/new" component={ SongCreate }/>
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 }
