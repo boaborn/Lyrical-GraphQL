e09d2fec708b44e0e1a7ca01948fbf7197df189b
boaborn <michaelzhaojq@hotmail.com>
2019-01-10 00:10:22 +1300

    fix not re-render after add a lyric SUPER IMPORTANT

--- client/index.js
+++ client/index.js
@@ -10,7 +10,15 @@ import SongList from './components/SongList'
 import SongCreate from './components/SongCreate'
 import SongDetail from './components/SongDetail'
 
-const client = new ApolloClient({})
+const client = new ApolloClient({
+  // This is a super important and confusing part
+  // This function fix when child component run mutation but not re-rendering issue
+  //  this function tells Apollo
+  //  1. Apollo go fetch data you need
+  //  2. look every single record
+  //  3. use the id field of that record to identifie that pice of data
+  dataIdFromObject: o => o.id
+})
 
 const Root = () => {
   return (
